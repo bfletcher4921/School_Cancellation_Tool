fe5e0b8d8212f42794a3589ab2c61c0250eefd5f
bfletcher4921 <bfletcher1@una.edu>
2024-03-28 20:47:09 -0500

    Add files via upload
    Initial code upload for school cancellation tool

---
+++ School cancellation tool.py
@@ -0,0 +1,19 @@
+#Brandi Fletcher 3/28/24
+
+#Random School Selection for Scheduling Priority Due to Cancellation
+
+import random
+
+#school list
+#list contains schools left to assign on 4/14/24
+
+schools = ["Athens", "Danville", "Decatur", "Hazel Green", "Mae Jeminson", "New Hope", "James Clemens"]
+
+
+school_selection = random.choice (schools)
+
+Print(school_selection)
+
+quit()
+
+	
