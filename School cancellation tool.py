d1e43920b33c79e075695e98395e8ce30da1d738
bfletcher4921 <bfletcher1@una.edu>
2024-03-28 21:29:16 -0500

    Create Loops and add Main

--- School cancellation tool.py
+++ School cancellation tool.py
@@ -2,18 +2,27 @@
 
 #Random School Selection for Scheduling Priority Due to Cancellation
 
-import random
+def main():
+    import random
 
 #school list
 #list contains schools left to assign on 4/14/24
 
-schools = ["Athens", "Danville", "Decatur", "Hazel Green", "Mae Jeminson", "New Hope", "James Clemens"]
+    schools = ["Athens", "Danville", "Decatur", "Hazel Green", "Mae Jeminson", "New Hope", "James Clemens"]
 
 
-school_selection = random.choice (schools)
-
-Print(school_selection)
-
-quit()
+#create loop to randomly select school
+    while True:
+        i = input("Press 'c' to randomly select a school or 'quit' to exit: ")
+        if i == 'quit':    
+            break
+        elif i!= 'c':
+            print("Invalid input")
+            continue
+        school_selection = random.choice(schools)
+        print(school_selection)
+    print("Done")
+__name__ == '__main__'
+main()
 
 	
